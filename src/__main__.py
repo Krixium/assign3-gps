32060c94f9e5faffe97ca7e89674cfee5f27f49f
Benny Wang <bennywang@localhost.localdomain>
2017-11-02 23:34:46 -0700

    Added GUI object that handles all gui tasks, currently cannot exit the application and text needs to be formatted in a cleaner way

--- src/__main__.py
+++ src/__main__.py
@@ -1,34 +1,22 @@
 if __name__ == "__main__":
-    import pygame
-    import sys
+    from src.Gui import Gui
+    from gps3 import gps3
 
-    from src.res import Colors, Meta
-    from src.gui import plot_coord
+    # Initialize GUI
+    gui = Gui()
 
-    run = True
+    gps_socket = gps3.GPSDSocket()
+    data_stream = gps3.DataStream()
 
-    pygame.init()
-    clock = pygame.time.Clock()
-    pygame.display.set_mode((Meta.WIDTH, Meta.HEIGHT))
-    screen = pygame.display.get_surface()
+    gps_socket.connect()
+    gps_socket.watch()
 
-    world = pygame.image.load(Meta.WORLD_MAP)
-    worldRect = world.get_rect()
+    for new_data in gps_socket:
+        if new_data:
+            data_stream.unpack(new_data)
 
-    screen.fill(Colors.BLACK)
-    screen.blit(world, worldRect)
-    plot_coord(screen, Colors.WHITE, 49.2827, -123.1207)    # Vancouver, Canada
-    plot_coord(screen, Colors.WHITE, 31.2304, 121.4737)     # Shanghai, China
-    plot_coord(screen, Colors.WHITE, -33.8688, 151.2093)    # Sydney, Australia
-    plot_coord(screen, Colors.WHITE, -22.9068, -43.1729)    # Rio de Janeiro, Brazil
-    plot_coord(screen, Colors.WHITE, -33.9249, 18.4241)     # Cape Town, South Africa
-    plot_coord(screen, Colors.WHITE, 51.5074, -0.1278)      # London, England
-    pygame.display.flip()
+            # pass data_stream object to gui to handle displaying information
+            gui.draw_screen(data_stream)
 
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        clock.tick(1000 / 30)
-    else:
-        print("Please run, do not import")
+else:
+    print("Please run, do not import")
